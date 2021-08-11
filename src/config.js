3782220b424f92395f45b21fe429edcf63c2d6ef
Vikash Kumar <wiryvikash15@gmail.com>
2021-08-11 20:55:22 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'Vikash Kumar | Software Engineer',
+  siteDescription:
+    'Vikash Kumar is a B.Tech Student at Jain University-FET,Bangalore who loves learning new things and helping tech fellow.',
+  siteKeywords:
+    'Vikash Kumar, Vikash, wiry, Vikash-Kumar-984, software engineer, Cyber Security, AWS,  web developer, javascript, wiry vikash, Bangalore',
+  siteUrl: 'https://github.com/Vikash-Kumar-984',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Vikash Kumar',
+  location: 'Bangalore, India',
+  email: 'wiryvikash15@gmail.com',
+  github: 'https://github.com/Vikash-Kumar-984',
+  twitterHandle: '@wiryvikash15',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/Vikash-Kumar-984',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/vikash-kumar-a071a0205/',
+    },
+    {
+      name: 'Codepen',
+      url: 'https://www.hackerrank.com/wiryvikash15/',
+    },
+    {
+      name: 'Instagram',
+      url: '',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/wiryvikash15',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
